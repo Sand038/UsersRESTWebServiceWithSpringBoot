8c5cea4410c10ef63c5eed0fa69491838f010d38
Sand <sandeepamadhusanka038@gmail.com>
2019-06-17 17:36:06 +0530

    Adding initial project structure

---
+++ src/main/java/com/sand/rest/webservices/userswebservice/UsersWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sand.rest.webservices.userswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersWebserviceApplication.class, args);
+    }
+
+}
